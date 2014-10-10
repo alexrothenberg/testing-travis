408280d4c7cb0e519f9c8b377524f3eb8c4204b6
Alex Rothenberg <alex@alexrothenberg.com>
2014-10-10 15:43:22 -0400

    travis

--- deploy-release.sh
+++ deploy-release.sh
@@ -12,6 +12,6 @@ Procfile
 sinatra.rb
  GITIGNORE >> .gitignore
  git add .
- git commit -m "Successful build #${TRAVIS_BUILD_NUMBER} https://travis-ci.org/${TRAVIS_REPO_SLUG}/builds/${TRAVIS_BUILD_ID} [Skip CI]"
+ git commit -m "Successful build ${TRAVIS_BUILD_NUMBER} https://travis-ci.org/${TRAVIS_REPO_SLUG}/builds/${TRAVIS_BUILD_ID} [Skip CI]"
  git push --force --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}" master:artifacts > /dev/null 2>&1
 )
