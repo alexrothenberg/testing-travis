2651108e072fc7e52eb7d8de4c968d02edfc2195
Alex Rothenberg <alex@alexrothenberg.com>
2014-10-10 15:36:48 -0400

    travis

--- deploy-release.sh
+++ deploy-release.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-( cd dist
+(
+ cd dist
  git init
  git config user.name "Travis-CI"
  git config user.email "travis@example.com"
  git add .
- git commit -m "Successful build ${TRAVIS_BUILD_NUMBER}"
- git push --force "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}" master:artifacts > /dev/null 2>&1
+ git commit -m "Successful build #${TRAVIS_BUILD_NUMBER} https://travis-ci.org/${TRAVIS_REPO_SLUG}/builds/${TRAVIS_BUILD_ID}"
+ git push --force --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}" master:artifacts > /dev/null 2>&1
 )
