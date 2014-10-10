cc1eaa04c2a5028cc10ea103321b3ebd67710a84
Alex Rothenberg <alex@alexrothenberg.com>
2014-10-10 15:50:03 -0400

    travis

--- deploy-release.sh
+++ deploy-release.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
 (
- cd dist
- git init
- git config user.name "Travis-CI"
- git config user.email "travis@example.com"
- echo <<< GITIGNORE
-Gemfile
-Gemfile.lock
-Procfile
-sinatra.rb
- GITIGNORE >> .gitignore
- git add .
- git commit -m "Successful build ${TRAVIS_BUILD_NUMBER} https://travis-ci.org/${TRAVIS_REPO_SLUG}/builds/${TRAVIS_BUILD_ID} [Skip CI]"
- git push --force --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}" master:artifacts > /dev/null 2>&1
+  git clone "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}" artifacts
+  cd artifacts
+  git checkout artifacts
+  rm -rf *
+  cp -r ../dist/* .
+  rm Gemfile Gemfile.lock Procfile sinatra.rb
+  git config user.name "Travis-CI"
+  git config user.email "travis@example.com"
+  git add .
+  git commit -m "Successful build ${TRAVIS_BUILD_NUMBER} https://travis-ci.org/${TRAVIS_REPO_SLUG}/builds/${TRAVIS_BUILD_ID} [Skip CI]"
+  git push --quiet origin:artifacts > /dev/null 2>&1
 )
