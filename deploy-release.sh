91194e768842e964893910805e79cfbad8e088b0
Travis-CI <travis@example.com>
2014-10-10 16:07:34 -0400

    travis

--- deploy-release.sh
+++ deploy-release.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-(
-  git clone "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}" artifacts
-  cd artifacts
-  git checkout artifacts
-  rm -rf *
-  cp -r ../dist/* .
-  rm Gemfile Gemfile.lock Procfile sinatra.rb
-  git config user.name "Travis-CI"
-  git config user.email "travis@example.com"
-  git add .
-  git commit -m "Successful build ${TRAVIS_BUILD_NUMBER} https://travis-ci.org/${TRAVIS_REPO_SLUG}/builds/${TRAVIS_BUILD_ID} [Skip CI]"
-  git push --quiet origin artifacts > /dev/null 2>&1
-)
+echo $TRAVIS_BRANCH
+
+if [ $TRAVIS_BRANCH -eq 'master' ]
+then
+  (
+    git clone "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}" artifacts
+    cd artifacts
+    git checkout artifacts
+    rm -rf *
+    cp -r ../dist/* .
+    rm Gemfile Gemfile.lock Procfile sinatra.rb
+    git config user.name "Travis-CI"
+    git config user.email "travis@example.com"
+    git add .
+    git commit -m "Successful build ${TRAVIS_BUILD_NUMBER} https://magnum.travis-ci.org/${TRAVIS_REPO_SLUG}/builds/${TRAVIS_BUILD_ID} [Skip CI]"
+    git push --quiet origin artifacts > /dev/null 2>&1
+  )
+else
+  echo "Not publishing artifacts on the '${TRAVIS_BRANCH}' branch"
+fi
